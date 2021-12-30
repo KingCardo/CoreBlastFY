59f45294c7d4cb2f0a790195f0742b5fb5cb749a
KingCardo <kingcardo@icloud.com>
2021-12-29 18:21:44 -0700

    updated os to 14

--- CoreBlastFY/CoreBlastFY/Models/Workout.swift
+++ CoreBlastFY/CoreBlastFY/Models/Workout.swift
@@ -27,21 +27,6 @@ struct Workout: Codable {
     var exercises: [Exercise]
     var isCustom = false
     
-//    var exercisesToReturn: [Exercise] {
-//        var exercises: [Exercise]
-//        switch user.totalPoints {
-//        case 0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87:
-//            exercises = self.exercises.filter( { $0.isSide == false })
-//        case 1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58,61,64,67,70,73,76,79,82,85,88:
-//            exercises = self.exercises.filter( { $0.isSide == true })
-//        case 2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59,62,65,68,71,74,77,80,83,86,89:
-//            exercises = self.exercises.filter( { $0.totalBody == true })
-//        default:
-//            exercises = self.exercises
-//        }
-//        return exercises
-//    }
-    
     var exercisesToReturn: [Exercise] {
         var exercises: [Exercise]
         switch user.totalPoints {
